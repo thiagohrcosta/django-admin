bea161e117a19b03acc53d31c46afc726fadbc0b
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2024-03-22 18:02:51 -0300

    Adding template page

--- djangoadmin/app1/urls.py
+++ djangoadmin/app1/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index, create
+from .views import index, create, edit
 
 urlpatterns = [
     path('', index, name='index'),
-    path('create', create, name='create')
+    path('create/', create, name='create'),
+    path('edit/<int:user_id>', edit, name='edit')
+
 ]
